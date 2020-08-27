c7ff98c0d40233b5b1c68c7f28cbfcbd512b44d4
Mohammed Alzaid <m.al-zaid@live.com>
2020-08-27 16:49:54 +0300

    completed

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,10 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
+        "my_list": [{"restaurant_name" : "Murad", "food_type" : "Turkish"},
+                    {"restaurant_name": "Kudu", "food_type": "Sandwich Sub"},
+                    {"restaurant_name": "McDonald's", "food_type": "Garbage"},
+                    ],
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +17,8 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
-
+        "my_object": {
+            "restaurant_name": "Murad", "food_type": "Turkish" , "price_range": "Cheap"
+        }
     }
     return render(request, 'detail.html', context)
